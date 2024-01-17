bda1b876598997a71028ede1639b36113237ceba
Enrico Blanco <enrico.cidadeb@gmail.com>
2024-01-17 19:26:26 -0300

    ajuste next auth

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,49 @@
-import React from 'react'
-import { About } from '../components/About'
+'use client'
+
+import React, { useState } from 'react'
+import { signIn } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 
 export default function Admin() {
-  return <About />
+  const [data, setData] = useState({
+    username: '',
+    password: ''
+  })
+
+  const router = useRouter()
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault()
+    signIn('credentials', {
+      ...data,
+      redirect: false
+    })
+    router.push('/about')
+  }
+
+  return (
+    <div className="w-full flex justify-center mt-20">
+      <form className="flex flex-col gap-4 max-w-md border-2 border-blue-400 rounded-md p-6">
+        <input
+          onChange={e => setData({ ...data, username: e.target.value })}
+          type="text"
+          placeholder="username"
+          className="outline-none"
+        />
+        <input
+          onChange={e => setData({ ...data, password: e.target.value })}
+          type="password"
+          placeholder="password"
+          className="outline-none"
+        />
+        <button
+          onClick={handleSubmit}
+          className="bg-blue-400 text-white rounded-lg"
+          type="submit"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  )
 }
---
+++ src/app/api/register/route.ts
@@ -0,0 +1,24 @@
+import bcrypt from 'bcrypt'
+import prisma from '@/libs/prismadb'
+import { NextResponse } from 'next/server'
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json()
+    const { username, password } = body
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const user = await prisma.user.create({
+      data: {
+        username,
+        password: hashedPassword
+      }
+    })
+
+    return NextResponse.json(user)
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({ error: 'Something went wrong' })
+  }
+}
