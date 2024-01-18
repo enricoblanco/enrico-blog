504d8cd88a302929cfd9a096e0b66c36aa90254d
Enrico Blanco <enrico.cidadeb@gmail.com>
2024-01-17 23:35:11 -0300

    volta os cao 2

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,8 @@
 'use client'
 
-import React, { useEffect } from 'react'
+import React from 'react'
 import { About } from '../components/About'
-import { useSession } from 'next-auth/react'
 
 export default function Posts() {
-  const { data: session, status } = useSession()
-
-  useEffect(() => {
-    console.log(session, status)
-  }, [session, status])
   return <About />
 }
